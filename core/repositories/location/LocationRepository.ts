a92056f76a7e90755917ef231b5ebc148ae4dcee
Kien Tran <kientt10@tcbs.com.vn>
2018-10-02 19:27:03 +0700

    update google place api

--- core/repositories/location/LocationRepository.ts
+++ core/repositories/location/LocationRepository.ts
@@ -6,25 +6,23 @@ import { createClient } from '@google/maps';
 export class LocationRepository implements LocationUsecaseInterface {
     searchCity(searchText: string): Observable<Location> {
         return Observable.create(function (observer) {
-            const client = createClient({ 
+            const client = createClient({
                 key: "AIzaSyDyp4K2oq2GDsHRIzy7Qpt87gGmlCRnvuI"
-           })
-
-            client.placesQueryAutoComplete(
-                {
-                    input: searchText
-                },
-                function(err, response) {
+            })
+            let query = {
+                input: searchText,
+                sessiontoken: "kienhiepsi"
+            }
+            client.placesAutoComplete(query,
+                function (err, response) {
 
                     if (response) {
-                        
-                        console.log(response.json.predictions)
 
-                        response.predictions.forEach(element => {
-                            console.log(element)
-                        });
+                        for (const prediction of response.json.predictions) {
 
-                        
+                           console.log(prediction)
+                        }
+        
                         observer.next(response)
                     } else {
                         console.log(err)
@@ -41,13 +39,13 @@ console.log("1")
 
 var repository = new LocationRepository()
 repository.searchCity("Ho")
-.subscribe((e) => {
-   
-    console.log(e)
-  
-},
-error => {
-    console.log(error)
+    .subscribe((e) => {
 
-}
-)
+        console.log(e)
+
+    },
+        error => {
+            console.log(error)
+
+        }
+    )
