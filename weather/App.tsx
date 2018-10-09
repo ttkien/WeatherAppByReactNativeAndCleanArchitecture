a81fca3a27fd4aa15bd38ad4069e4f3e05607bc7
Kien Tran <kientt10@tcbs.com.vn>
2018-10-09 10:05:39 +0700

    fix dependencies

--- weather/App.tsx
+++ weather/App.tsx
@@ -6,58 +6,72 @@
  * @flow
  */
 
-import React from 'react';
-import { Component } from 'react'
-import { Platform, StyleSheet, Text, View, Image, SafeAreaView, Alert, FlatList } from 'react-native';
-import { Card, ListItem, Button, Icon, SearchBar } from 'react-native-elements'
-import { Location } from 'weather-domain'
-import { DefaultLocationRepository } from 'weather-repositories'
-import * as Rx from 'rxjs'
-import { map, throttle, throttleTime } from 'rxjs/operators'
-import {SearchLocationList, SearchLocationListDataItem} from 'weather-ui';
+import React from "react";
+import { Component } from "react";
+import { Alert, FlatList, Image, Platform, SafeAreaView, StyleSheet, Text, View  } from "react-native";
+import { Button, Card, Icon, ListItem, SearchBar } from "react-native-elements";
+import * as Rx from "rxjs";
+import { map, throttle, throttleTime } from "rxjs/operators";
+import { Location } from "weather-domain";
+import { DefaultLocationRepository } from "weather-repositories";
+import {SearchLocationList, SearchLocationListDataItem} from "weather-ui";
 
-type Props = {};
-type State = {
-    locations: SearchLocationListDataItem[]
-};
-export default class App extends Component<Props,State> {
+class Props {}
 
+// tslint:disable-next-line:max-classes-per-file
+interface IState {
+     locations: SearchLocationListDataItem[];
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export default class App extends Component<Props, IState> {
+
+    public state = {
+        locations: [],
+    };
     constructor(props: Props) {
         super(props);
     }
-
-    state = {
-        locations: []
-    }
-    onChangeText = (text) => {
-
-        var repository = new DefaultLocationRepository()
+    public onChangeText = (text) => {
+        const repository = new DefaultLocationRepository();
         repository.searchCity(text)
             .pipe(throttleTime(1000))
             .subscribe(
-                (results:[Location]) => {
-                    console.log(results)
-                    const dataItems = results.map((location:Location) => {
-                        return new SearchLocationListDataItem(location.name)
-                    })
+                (results: [Location]) => {
+                    const dataItems = results.map((location: Location) => {
+                        return new SearchLocationListDataItem(location.name);
+                    });
                     this.setState({
-                        locations: dataItems
-                    })
-                }
-            )
+                        locations: dataItems,
+                    });
+                },
+            );
     }
 
-    render() {
+    public onClear = () => {
+
+        // alert("onClear");
+        this.onChangeText("");
+    }
+    public onCancel = () => {
+
+        // this.onChangeText("");
+    }
+
+    public render() {
         return (
             <SafeAreaView>
                 <SearchBar
+                    platform="ios"
                     onChangeText={this.onChangeText}
-                    placeholder='Enter location'
+                    placeholder="Enter location"
+                    onClear={this.onClear}
+                    onCancel={this.onCancel}
                 ></SearchBar>
                 <SearchLocationList
                 items={this.state.locations}
                 />
             </SafeAreaView>
-        )
+        );
     }
 }
