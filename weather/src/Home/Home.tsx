aa52c5296aca6f0a147205763acea78caa6e5d6d
Kien Tran <kientt10@tcbs.com.vn>
2018-10-10 10:32:39 +0700

    improve list view performance

--- weather/src/Home/Home.tsx
+++ weather/src/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Platform } from 'react-native';
 import { SearchBar } from "react-native-elements";
 import { connect } from "react-redux";
 import { SearchLocationList, SearchLocationListDataItem } from "weather-ui";
@@ -8,17 +8,38 @@ import { searchLocation } from "../actions/actions";
 import {  ISearchLocationState } from "../reducer/reducer";
 // import reducer from "../reducer/reducer";
 
-export class Home extends Component {
+class Props {
+
+    public items: SearchLocationListDataItem[];
+    public error: Error | null;
+    public isLoading: boolean;
+
+    constructor(items: SearchLocationListDataItem[], error: Error | null = null, isLoading: boolean) {
+        this.items = items;
+        this.error = error;
+        this.isLoading = isLoading;
+    }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export class Home extends Component<Props> {
 
     public onChangeText = (text: string) => {
         this.props.searchLocation(text);
+    }
+
+    public onCancel = () => {
+        this.onChangeText("");
     }
 
     public render() {
         return (
             <SafeAreaView>
                 <SearchBar
+             platform="ios"
                     onChangeText={this.onChangeText}
+                    onCancel={this.onCancel}
+                    onClear={this.onCancel}
                     placeholder="Enter location"
                 ></SearchBar>
                 <SearchLocationList
@@ -29,7 +50,7 @@ export class Home extends Component {
     }
 }
 
-const mapStateToProps = (state: ISearchLocationState) => {
+const mapStateToProps = (state: ISearchLocationState): Props => {
     // tslint:disable-next-line:no-shadowed-variable
     const searchLocation: ISearchLocationState = state;
 
@@ -41,11 +62,17 @@ const mapStateToProps = (state: ISearchLocationState) => {
     }
 
     return {
-        items: locations,
+        items: addKeysToItems(locations),
         error: searchLocation.error,
         isLoading: searchLocation.isLoading
     };
 };
+
+function addKeysToItems(params: SearchLocationListDataItem[]) {
+    return params.map((item: SearchLocationListDataItem) => {
+        return Object.assign(item, {key: item.title});
+    });
+}
 
 const mapDispatchToProps = {
     searchLocation
