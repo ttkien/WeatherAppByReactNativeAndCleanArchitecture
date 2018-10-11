6ff05ca046d9b0556db13ccb5c395413a5598d8c
Kien Tran <kientt10@tcbs.com.vn>
2018-10-11 15:21:02 +0700

    set up react-native-navigation module

--- repositories/lib/location/LocationRepository.ts
+++ repositories/lib/location/LocationRepository.ts
@@ -1,44 +1,48 @@
-import * as RxJs from 'rxjs';
-import * as RxJsOperators from 'rxjs/operators'
+import * as RxJs from "rxjs";
+import * as RxJsOperators from "rxjs/operators";
 
-import { Location, LocationRepository } from 'weather-domain';
 import Axios, * as axios from "axios";
-import { createClient } from 'react-native-google-maps-services';
+import { createClient } from "react-native-google-maps-services";
+import { Location, LocationRepository } from "weather-domain";
 // import { map, flatMap } from 'rxjs/operators';
 // import {} from '@reactivex/rxjs'
 // import 'rxjs/Rx'
 export class DefaultLocationRepository implements LocationRepository {
-    searchCity(searchText: string): RxJs.Observable<Location> {
-        return RxJs.Observable.create(function (observer) {
-            const key = "AIzaSyBO35s72ZQbDBabY1y9JsW59SBsHKcIfC8"
-            const uri = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key="+ key +"&input=" + searchText + "&types=(cities)"
+    public searchCity(searchText: string): RxJs.Observable<Location> {
+        return RxJs.Observable.create((observer) => {
+            const key = "AIzaSyDz8iOOAc-1NqzKRLl5n1qJFYY39NoL6EY";
+            const endcodedSearchText = encodeURI(searchText);
+            const uri = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=" + key
+            + "&input=" + endcodedSearchText + "&types=(cities)";
 
-            
             // axios.default.defaults.headers["referer"] = "http://kienhiepsi.com"
             axios.default.get(uri)
-            .then( response => {
-                if (response.status == 200) {
-                    var results: Array<Location> = []
-                    console.log(response.data)
-                                for (const prediction of response.data["predictions"]) {
-                                    var structured_formatting = prediction.structured_formatting
-                                    var mainText = structured_formatting.main_text
-                                    var secondaryText = structured_formatting.secondary_text
-        
+            .then( (response) => {
+                if (response.status === 200 && !response.data.error_message) {
+                    const results: Location[] = [];
+                    console.info(response.data);
+                    for (const prediction of response.data.predictions) {
+                                    const structured_formatting = prediction.structured_formatting;
+                                    const mainText = structured_formatting.main_text;
+                                    const secondaryText = structured_formatting.secondary_text;
+
                                     results.push(new Location(
-                                        mainText, secondaryText
-                                    ))
+                                        mainText, secondaryText,
+                                    ));
                                 }
-        
-                                observer.next(results)
+
+                    observer.next(results);
+                } else {
+                    const error = new Error(response.data.error_message);
+                    observer.error(error);
                 }
                 // console.log(response); // Show the JSON response object.
             })
-            .catch( error => {
-                console.log(error); // Show the JSON response object.
+            .catch( (error) => {
+                console.info(error); // Show the JSON response object.
 
-                observer.error(error)
-            })
+                observer.error(error);
+            });
 
             // RxHR.get(uri, options).subscribe(
             //     data => {
@@ -58,15 +62,14 @@ export class DefaultLocationRepository implements LocationRepository {
             //         }
             //     },
             //     error => {
-             
+
             //     }
             // )
         // })
-        })
+        });
     }
-    
-}
 
+}
 
 // var repository = new DefaultLocationRepository()
 // repository.searchCity('ho').subscribe(
--- weather/src/Home/Home.tsx
+++ weather/src/Home/Home.tsx
@@ -1,81 +1,107 @@
 import { Component } from "react";
 import React from "react";
-import { SafeAreaView, Platform } from 'react-native';
+import { Image, Platform, SafeAreaView, Text, KeyboardAvoidingView } from "react-native";
 import { SearchBar } from "react-native-elements";
 import { connect } from "react-redux";
 import { SearchLocationList, SearchLocationListDataItem } from "weather-ui";
 import { searchLocation } from "../actions/actions";
-import {  ISearchLocationState } from "../reducer/reducer";
+import { ISearchLocationState } from "../reducer/reducer";
 // import reducer from "../reducer/reducer";
 
 class Props {
+  public items: SearchLocationListDataItem[];
+  public error: Error | null;
+  public isLoading: boolean;
 
-    public items: SearchLocationListDataItem[];
-    public error: Error | null;
-    public isLoading: boolean;
-
-    constructor(items: SearchLocationListDataItem[], error: Error | null = null, isLoading: boolean) {
-        this.items = items;
-        this.error = error;
-        this.isLoading = isLoading;
-    }
+  constructor(
+    items: SearchLocationListDataItem[],
+    error: Error | null = null,
+    isLoading: boolean
+  ) {
+    this.items = items;
+    this.error = error;
+    this.isLoading = isLoading;
+  }
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class Home extends Component<Props> {
+  public static navigationOptions = {
+    drawerLabel: "Home",
+    drawerIcon: ({ tintColor }) => (
+      <Image source={require("./icon-home.png")} style={[{ tintColor }]} />
+    )
+  };
 
-    public onChangeText = (text: string) => {
-        this.props.searchLocation(text);
+  public func;
+  public onChangeText = (text: string) => {
+    this.props.searchLocation(text);
+  };
+
+  public onCancel = () => {
+    this.onChangeText("");
+  };
+  public isError(): boolean {
+    return this.props.error !== undefined && this.props.error !== null;
+  }
+
+  public render() {
+    let errorView;
+    if (this.isError()) {
+      errorView = <Text>{this.props.error.message}</Text>;
     }
 
-    public onCancel = () => {
-        this.onChangeText("");
-    }
-
-    public render() {
-        return (
-            <SafeAreaView>
-                <SearchBar
-             platform="ios"
-                    onChangeText={this.onChangeText}
-                    onCancel={this.onCancel}
-                    onClear={this.onCancel}
-                    placeholder="Enter location"
-                ></SearchBar>
-                <SearchLocationList
-                    items={this.props.items}
-                />
-            </SafeAreaView>
-        );
-    }
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <KeyboardAvoidingView
+          style = {{ flex: 1 }}
+          behavior="padding"
+          enabled
+        >
+          <SearchBar
+            platform="ios"
+            onChangeText={this.onChangeText}
+            onCancel={this.onCancel}
+            onClear={this.onCancel}
+            placeholder="Enter location"
+          />
+          {errorView}
+          <SearchLocationList items={this.props.items} />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    );
+  }
 }
 
 const mapStateToProps = (state: ISearchLocationState): Props => {
-    // tslint:disable-next-line:no-shadowed-variable
-    const searchLocation: ISearchLocationState = state;
+  // tslint:disable-next-line:no-shadowed-variable
+  const searchLocation: ISearchLocationState = state;
 
-    let locations: SearchLocationListDataItem[] = [];
-    if (searchLocation && searchLocation.locations) {
-        locations = searchLocation.locations.map((location) => {
-            return new SearchLocationListDataItem(location.name);
-        });
-    }
+  let locations: SearchLocationListDataItem[] = [];
+  if (searchLocation && searchLocation.locations) {
+    locations = searchLocation.locations.map(location => {
+      return new SearchLocationListDataItem(location.name);
+    });
+  }
 
-    return {
-        items: addKeysToItems(locations),
-        error: searchLocation.error,
-        isLoading: searchLocation.isLoading
-    };
+  return {
+    items: addKeysToItems(locations),
+    error: searchLocation.error,
+    isLoading: searchLocation.isLoading
+  };
 };
 
 function addKeysToItems(params: SearchLocationListDataItem[]) {
-    return params.map((item: SearchLocationListDataItem) => {
-        return Object.assign(item, {key: item.title});
-    });
+  return params.map((item: SearchLocationListDataItem) => {
+    return Object.assign(item, { key: item.title });
+  });
 }
 
 const mapDispatchToProps = {
-    searchLocation
+  searchLocation
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
